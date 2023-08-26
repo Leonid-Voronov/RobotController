2e704248087647ee40ae06ca7009212de69baca3
torn2706 <torn0627@gmail.com>
2023-08-26 03:54:20 +0200

    basic surface alignment

--- Robot/Assets/Scripts/PlayerGroundChecker.cs
+++ Robot/Assets/Scripts/PlayerGroundChecker.cs
@@ -18,22 +18,27 @@ namespace TPD
         
         private bool grounded;
         private bool onSlope;
-        private Vector3 slopeHitNormal;
+        private Vector3 hitNormal;
         public bool Grounded => grounded;
+        public Vector3 HitNormal => hitNormal;
+        
 
         private void FixedUpdate()
         {
-            grounded = Physics.Raycast(transform.position, Vector3.down, out RaycastHit slopeHit , playerHeight * .5f + raycastExtension, groundMask);
+            grounded = Physics.Raycast(transform.position, -rb.transform.up, out RaycastHit slopeHit , playerHeight * .5f + raycastExtension, groundMask);
 
             if (grounded)
             {
                 rb.drag = groundDrag;
+                hitNormal = slopeHit.normal;
             }
             else
             {
                 rb.drag = 0;
             }
         }
+
+         
     }
 }
 
